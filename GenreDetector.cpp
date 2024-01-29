d5d3a958b5274e769b23c329588697b23aabba90
pya-h <pya.py@gmail.com>
2024-01-29 18:28:26 +0330

    Starting Out...

---
+++ GenreDetector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+// Structs & Models:
+
+// Function Prototypes:
+
+// Main:
+int main()
+{
+
+    // Data Preprocessing:
+
+    string command = "";
+
+    // Menu & Main App Loop
+    while (command != "exit")
+    {
+
+        // Command Check:
+
+    }
+    return 0;
+}
+
+// Function Declerations:
